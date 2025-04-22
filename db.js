b9fdcf37bfb360a04bbaae587741e082790aa695
manshibhatt <bhattmanshi66@gmail.com>
2025-04-22 11:05:01 +0530

    Update MySQL config for Clever Cloud

--- db.js
+++ db.js
@@ -1,10 +1,13 @@
 import mysql from "mysql2/promise";
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'manshi',
-  database: 'mysql_db',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 
